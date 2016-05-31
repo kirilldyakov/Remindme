952b060958c352d3f03b6a52190b89a474602198
2l8 <dyakovks@gmail.com>
2016-05-31 14:09:10 +0300

    [DKS-103] Implemented Navigation view

--- app/src/main/java/com/example/user/remindme/MainActivity.java
+++ app/src/main/java/com/example/user/remindme/MainActivity.java
@@ -1,22 +1,21 @@
 package com.example.user.remindme;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.example.user.remindme.R;
 
-/**
- * Created by user on 30.05.16.
- */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     int LAYOUT = R.layout.activity_main;
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(LAYOUT);
         initToolbar();
@@ -24,6 +23,8 @@ public class MainActivity extends Activity {
 
     private void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        if(toolbar==null) return;
         toolbar.setTitle(R.string.app_name);
 
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
