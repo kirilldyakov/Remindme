b343afaba2f2f5ec81c6e9dc2802d641565f3d46
Kirill Dyakov <Dyakovks@gmail.com>
2016-05-31 21:27:51 +0300

    Start to implement NavigationView

---
+++ app/src/main/java/com/example/user/remindme/Constants.java
@@ -0,0 +1,9 @@
+package com.example.user.remindme;
+
+class Constants {
+
+    public static final int TAB_ONE     = 0;
+    public static final int TAB_TWO     = 1;
+    public static final int TAB_THREE   = 2;
+
+}
--- app/src/main/java/com/example/user/remindme/MainActivity.java
+++ app/src/main/java/com/example/user/remindme/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.user.remindme;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-import com.example.user.remindme.R;
+import com.example.user.remindme.adapter.TabsPagerFragmentAdapter;
 
 public class MainActivity extends AppCompatActivity {
     private static final int LAYOUT = R.layout.activity_main;
 
     private Toolbar toolbar;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(LAYOUT);
         initToolbar();
         initNavigationView();
+        //initTabLayout();
+        initTabs();
     }
 
     private void initToolbar() {
@@ -40,12 +47,56 @@ public class MainActivity extends AppCompatActivity {
         toolbar.inflateMenu(R.menu.menu);
     }
 
-
     private void initNavigationView() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.view_navigation_open, R.string.view_navigation_close );
+
+        assert drawerLayout != null;
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        assert navigationView != null;
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+                switch (menuItem.getItemId()){
+                    case R.id.actionNotificationItem:
+                        showNotificationTab();
+                }
+
+                return true;
+            }
+        });
+
+    }
+
+    private void initTabs() {
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        assert viewPager != null;
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        assert tabLayout != null;
+        tabLayout.setupWithViewPager(viewPager);
+
+/*        viewPager.setOnClickListener(new View.OnClickListener() {
+                                         @Override
+                                         public void onClick(View v) {
+                                             Toast t = Toast.makeText(getApplicationContext(), Integer.toString(v.getId()), Toast.LENGTH_LONG);
+                                             t.show();
+                                         }
+                                     }
+        );*/
+    }
+
+    private void showNotificationTab() {
+        viewPager.setCurrentItem(Constants.TAB_TWO);
 
     }
 }
-
-//https://youtu.be/6GXAZupRGlY?list=PLIU76b8Cjem54jkj0XtCAp8JqZooY_eBI&t=428
-//https://youtu.be/k9dStNYqNXg?list=PLIU76b8Cjem54jkj0XtCAp8JqZooY_eBI
